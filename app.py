6552582611f171c6dd1718c422223551ddb0931c
Sai Praneeth <praneeth.sai2008@gmail.com>
2022-11-18 12:38:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def wel_msg():
-    # color = os.environ.get('COLOR', "green") 
-    message = "Hello from the App's Docker image deployed on Minishift" 
+def main():
+    color = os.environ.get('COLOR', "green") 
+    message = "Welcome !!! This is "+color+" environment" 
     return message
 
 @app.route('/how are you')
@@ -13,4 +13,4 @@ def hello():
     return 'I am good, how about you?'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(host="0.0.0.0", port=8080)
